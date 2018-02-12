d29e403e3f90ff2a72e98d8a9154ebe6b0cf1382
Tal Yaacovi <talyaacovi@gmail.com>
2018-02-12 15:39:25 -0800

    Map recenters when user clicks on list item and displays info window.

--- static/js/maps.js
+++ static/js/maps.js
@@ -1,50 +1,90 @@
 "use strict";
 
-let city = {lat: $('#city').data('lat'), lng: $('#city').data('lng')};
+function initMap() {
 
-let map = new google.maps.Map(document.querySelector('#map'), {
-    center: city,
-    zoom: 12,
-    mapTypeControl: false,
-});
+    // get latitude and longitude for the city
+
+    let city = {lat: $('#city').data('lat'), lng: $('#city').data('lng')};
+
+    // google map with city as center
+    window.map = new google.maps.Map(document.querySelector('#map'), {
+        center: city,
+        zoom: 12,
+        mapTypeControl: false,
+    });
+
+    let infoWindow = new google.maps.InfoWindow({
+        width: 150
+    });
+
+    // get array of all restaurants in list
+    let restArray = $('#top-restaurants li');
+    let marker, html, restaurant;
+    let newLat, newLng, newCenter;
+
+    // iterate over restaurants in array
+    for (let i = 0; i < restArray.length; i++) {
+        let lat = restArray[i].dataset.lat;
+        let lng = restArray[i].dataset.lng;
+        // restArray[i].addEventListener('click', changeMapCenter);
 
 
+        // create map marker for the specific restaurant
+        marker = new google.maps.Marker({
+                    position: new google.maps.LatLng(lat, lng),
+                    map: map,
+                    title: 'Hover text',
+                    // icon: myImageURL
+                });
 
+        // define contents for info window
+        html = (
+              '<div class="window-content">' +
+                    '<p>' + restArray[i].innerText + '</p>' +
+              '</div>');
 
+        restaurant = restArray[i];
 
-function addMarker(lat, lng) {
-    // let myImageURL = 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png';
-    let nearSydney = new google.maps.LatLng(lat, lng);
-    let marker = new google.maps.Marker({
-      position: nearSydney,
-      map: map,
-      title: 'Hover text',
-  });
-  return marker;
+        // call function to bind info window to map
+        bindInfoWindow(marker, map, infoWindow, html);
+        changeMapCenter(restaurant, marker, map, infoWindow, html);
+
+    }
+
+    // function adds event listener to markers, closes any opened when one is
+    // clicked, sets content based on passed in html, opens info window with
+    // new content on the marker that was clicked.
+    function bindInfoWindow(marker, map, infoWindow, html) {
+        google.maps.event.addListener(marker, 'click', function () {
+            infoWindow.close();
+            infoWindow.setContent(html);
+            infoWindow.open(map, marker);
+        });
+    }
+
+    function changeMapCenter(restaurant, marker, map, infoWindow, html) {
+        restaurant.addEventListener('click', function () {
+            infoWindow.close();
+            infoWindow.setContent(html);
+            infoWindow.open(map, marker);
+
+            // let newLat = parseFloat(evt.target.dataset.lat);
+            // let newLng = parseFloat(evt.target.dataset.lng);
+            newLat = parseFloat(restaurant.getAttribute('data-lat'));
+            newLng = parseFloat(restaurant.getAttribute('data-lng'));
+
+            newCenter = {lat: newLat, lng: newLng};
+            map.setCenter(newCenter);
+            map.setZoom(14);
+        });
+
+    }
+
 }
 
-let restArray = $('#top-restaurants li');
-
-for (let i = 0; i < restArray.length; i++) {
-    let lat = restArray[i].dataset.lat;
-    let lng = restArray[i].dataset.lng;
-
-    addMarker(lat, lng);
-}
 
 
-function addInfoWindow() {
 
-  let contentString = '<div id="content">' +
-    '<h1>All my custom content</h1>' +
-    '</div>';
+google.maps.event.addDomListener(window, 'load', initMap);
 
-  let infoWindow = new google.maps.InfoWindow({
-    content: contentString,
-    maxWidth: 200
-  });
 
-  marker.addListener('click', function() {
-    infoWindow.open(map, marker);
-  });
-}
