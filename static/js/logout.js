e0035c8aacc318464af36650de6fedf91dc530f1
Tal Yaacovi <talyaacovi@gmail.com>
2018-03-03 13:05:14 -0800

    Added bootstrap alerts to city page, homepage, and profile page.

--- static/js/logout.js
+++ static/js/logout.js
@@ -38,11 +38,14 @@ function loginMessage(result) {
         $('#profile-page').attr('href', '/users/' + result.user);
         if (result.isActive === 'True') {
             isActive = true; // global variable defined in base.html
-            $('#msg-para').html('You have successfully logged in!');
+            $('#homepage-alert').show();
+            $('#homepage-alert').append('You have successfully logged in!');
         }
         else {
             $('#search-div').hide();
-            $('#msg-para').html('You have successfully logged in! Please add at least 5 restaurants to your <a href="/users/' + result.user + '/favorites">Favorites</a> list to access more content.');
+            $('#view-controls').hide();
+            $('#homepage-alert').show();
+            $('#homepage-alert').append('You have successfully logged in! Please add at least 5 restaurants to your <a href="/users/' + result.user + '/favorites">Favorites</a> list to access more content.');
         }
     }
     else if (result == 'Incorrect') {
@@ -187,7 +190,9 @@ function signupUser(result) {
         $('#profile-page').attr('href', '/users/' + result);
         $('#search-div').hide();
         $('#icons').hide();
-        $('#msg-para').html('Your account has been created! Please add at least 5 restaurants to your <a href="/users/' + result + '/favorites">Favorites</a> list to access more content.');
+        $('#view-controls').hide();
+        $('#homepage-alert').show();
+        $('#homepage-alert').append('Your account has been created! Please add at least 5 restaurants to your <a href="/users/' + result + '/favorites">Favorites</a> list to access more content.');
 
     }
 
